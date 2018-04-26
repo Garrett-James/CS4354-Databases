00916872be99e7aea0e5245d995a33103e880116
Chavez-Saul <chavez5s18@gmail.com>
2018-04-25 21:15:03 -0500

    sql code

---
+++ MySqlCode.sql
@@ -0,0 +1,23 @@
+CREATE SCHEMA IF NOT EXISTS `FruitDatabase` DEFAULT CHARACTER SET utf8 ;
+USE `FruitDatabase` ;
+
+CREATE TABLE `consumer_info` (
+  `year` int(11) NOT NULL,
+  `consumption` double NOT NULL,
+  PRIMARY KEY (`year`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `priceinfo` (
+  `year` int(11) NOT NULL,
+  `price_info` double NOT NULL,
+  `price_grower` double NOT NULL,
+  PRIMARY KEY (`year`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `stateproduction` (
+  `year` int(11) NOT NULL,
+  `state` varchar(45) NOT NULL,
+  `yield` double NOT NULL,
+  `bearingAcre` double NOT NULL,
+  PRIMARY KEY (`year`,`state`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
